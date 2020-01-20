7989a1a91a351d2ec9fa0d5591bc35abef909793
R-SAIKI <tateishiryu1357@gmail.com>
2020-01-20 23:51:08 +0900

    とりあえずHelloWorldまでOK!

---
+++ mygroupware/src/test/java/rsrepo/groupware/mygroupware/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rsrepo.groupware.mygroupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
